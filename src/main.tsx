38f948660c0ca26f80a5f1ad0dd79bad81acad84
jironim0 <nadir_garipov@mail.ru>
2026-02-04 18:47:20 +0300

    Switch to HashRouter for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,13 @@ import './index.scss'
 import App from './app/ui/App/App.tsx'
 import { Provider } from 'react-redux'
 import { store } from './app/model/store.ts'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 
 )
